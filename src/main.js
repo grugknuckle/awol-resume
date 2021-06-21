b78916cd20af3c40f115b1ec0ffe38ad38330dda
Grugknuckle <awol@bete.com>
2021-06-21 17:22:37 -0400

    no audience

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from '@/App.vue'
 import router from './router'
 // import store from './store'
+
 import vuetify from '@/plugins/vuetify'
 import helpers from '@/plugins/helper'
 import { Auth0Plugin } from '@/plugins/auth0'
@@ -11,8 +12,7 @@ Vue.use(helpers)
 Vue.use(Auth0Plugin, {
   domain: process.env.VUE_APP_AUTH0_DOMAIN,
   clientId: process.env.VUE_APP_AUTH0_CLIENT_ID,
-  audience: process.env.VUE_APP_AUTH0_AUDIENCE,
-  // redirectUri: process.env.VUE_APP_DOMAIN,
+  // audience: process.env.VUE_APP_AUTH0_AUDIENCE,
   onRedirectCallback: (appState) => {
     router.push(appState && appState.targetUrl ? appState.targetUrl : window.location.pathname)
   }
