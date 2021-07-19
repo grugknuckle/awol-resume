df120b2554504752ec18eaf72cc5b816a8cb13f0
Grugknuckle <awol@bete.com>
2021-07-19 11:38:34 -0400

    enforce https

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ const serveStatic = require('serve-static')
 const path = require('path')
 
 const app = express()
+const ENVIRON = process.env.NODE_ENV
+
+if(ENVIRON === 'production') {
+  app.use(enforceHTTPS)
+}
 
 //here we are configuring dist to serve app files
 app.use('/', serveStatic(path.join(__dirname, '/dist')))
@@ -16,3 +21,15 @@ const port = process.env.PORT || 8080
 app.listen(port, () => {
   console.log(`app is listening on port: ${port}`)
 })
+
+
+function enforceHTTPS(req, res, next) {
+  const scheme = req.headers['x-forwarded-proto']
+  const host = req.headers.host
+  const url = req.url
+  if (scheme !== 'https') {
+    res.redirect(`https://${host}${url}`)
+  } else {
+    next()
+  }
+}
